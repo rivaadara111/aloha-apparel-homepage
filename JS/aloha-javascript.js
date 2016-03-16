c6e3333e45a180c1b31f40bab4391caf5b9560b5
Riva Adara Ridley <rivaridley1@Riva-Ridleys-MacBook-Pro-2.local>
2016-03-16 10:44:01 -0700

    CSS Tweaks.

--- JS/aloha-javascript.js
+++ JS/aloha-javascript.js
@@ -1,41 +1,60 @@
-// BX SLIDEYDOOPERPOOP
-
-$(function(){
-	$('.bxslider').bxSlider({
-		speed: 500,
-		controls:false,
-		auto:true,
-		minSlides:1,
-		maxSlides:4,
-		moveSlides:1,
-		slideMargin:0
-	});
+// BX SLIDER
+$(function() {
+  $('.bxslider').bxSlider({
+    speed: 500,
+    controls: false,
+    auto: true,
+    minSlides: 1,
+    maxSlides: 4,
+    moveSlides: 1,
+    slideMargin: 0
+  });
 });
 
-//POP UP function
-
- $('.subscribe > input:submit').click(function(event){
- event.preventDefault();
- alert('Thanks for subscribing!');
- });
-
-
-//SMOOTH SCROLLZDAWG
-
+//FORM POP UP function
 $(function() {
-      $('a[href*=#]:not([href=#])').on('click', function() {
-         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  $(document).ready(function() {
+    $('.subscribe > input:submit').click(function(event) {
 
-            if (target.length) {
-               $('html,body').animate({
-                  scrollTop: target.offset().top
-               }, 500);
+      var email = $('#email').val();
 
-               return false;
-            }
-         }
+      event.preventDefault();
+
+      if ($(email) == '') {
+        alert('Please enter a valid email address.');
+      } else if (validateEmail(email) === re) {
+        alert('Thanks for subscribing!');
+      } else {
+        alert('Something has gone wrong. Please try again later.');
+      }
+    });
+
+  });
+// Function that validates email address through a regular expression.
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+  //SMOOTH SCROLLING http://www.paulund.co.uk/smooth-scroll-to-internal-links-with-jquery
+
+  $(function() {
+
+    $('a[href^="#"]').on('click', function(event) {
+      event.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+      }, 900, 'swing', function() {
+        window.location.hash = target;
       });
-   });
+    });
 
+  });
+
+  //doc ready closing brackets
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+#Aloha Apprel
+
+-kind of work you did, why someone should care about it.
